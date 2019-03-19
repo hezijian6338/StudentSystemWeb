510bb4b6d99d25e5d7e03ce94ab2438158d2562b
hezijian6338 <627222344@qq.com>
2019-03-19 16:57:12 +0800

    添加注释在student的API种

--- src/api/student.js
+++ src/api/student.js
@@ -1,30 +1,35 @@
 import request from '@/utils/request'
 // import qs from 'qs'
 
+// TODO: 根据学生的学号返回学生信息
 export function studentInfo(studentno) {
   return request({
     url: '/students/studentno/' + studentno,
     method: 'get'
   })
 }
+// TODO: 根据学生的学号来获取可选课程的信息
 export function courseList(studentno) {
   return request({
     url: '/students/' + studentno + '/courseInfos',
     method: 'get'
   })
 }
+// TODO: 根据课程的id来获取课程的详细信息
 export function course(courseid) {
   return request({
     url: '/course/info/id/' + courseid,
     method: 'get'
   })
 }
+// TODO: 已选课程(根据学生的学号查询)
 export function selectedCourses(studentno) {
   return request({
     url: '/course/info/student/' + studentno,
     method: 'get'
   })
 }
+// TODO: 已选导师(根据学生的学号查询)
 export function selectedGuidTeacher(studentno) {
   return request({
     url: '/learningguid/studentslist/studentno/' + studentno,
